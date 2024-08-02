e1cd26a0b5c12ab355ba392d8acc99f0585d9109
RenanYMartins <yohanssen.martins@gmail.com>
2024-08-02 20:37:52 -0300

    :sparkles: swagger

--- Aula_22_Swagger/src/app.ts
+++ Aula_22_Swagger/src/app.ts
@@ -1,8 +1,20 @@
 import express from 'express';
+import { RegisterRoutes } from './route/routes';
+import { setupSwagger } from './config/swagger';
+
 
 const app = express();
 
 const PORT = 3040;
+
+const apiRouter = express.Router();
+RegisterRoutes(apiRouter);
+
+app.use('/api', apiRouter);
+
+RegisterRoutes(app);
+
+setupSwagger(app);
 
 app.use(express.json());
 
